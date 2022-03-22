23fbd727dc99713d35ba874d5645ac2e12a37b38
skybedy <skybedy@gmail.com>
2022-03-22 11:35:48 +0100

    220222-2

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -39,9 +39,10 @@ class BlogPost
     private $category;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $name;
+
 
 
     public function getId(): ?int
@@ -96,12 +97,13 @@ class BlogPost
 
         return $this;
     }
+
     public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = $name;
 
